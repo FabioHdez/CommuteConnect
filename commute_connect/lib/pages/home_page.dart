0da4de623d056436dcb7222745df5e88998ce346
Fabio Hernandez <44535804+FabioHdez@users.noreply.github.com>
2024-02-16 17:56:36 -0500

    Revert "Merge branch 'Navigation-Bar'"
    This reverts commit 544c74a0742a70de18ce44fba797e1d3b4ef1f89, reversing
    changes made to 878822cdbe813722848a19dd470e99d9216c0f5b.

--- commute_connect/lib/pages/home_page.dart
+++ commute_connect/lib/pages/home_page.dart
@@ -1,90 +1,34 @@
-//Homepage, user is logged in.
+//Homepage, user is logged in. 
 
-import 'package:commute_connect/pages/create_page.dart';
-import 'package:commute_connect/pages/profile_page.dart';
-import 'package:commute_connect/pages/search_page.dart';
-import 'package:commute_connect/pages/travel_plans_page.dart';
-//import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:google_nav_bar/google_nav_bar.dart';
 
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+class HomePage extends StatelessWidget {
+  HomePage({super.key});
 
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  //final user = FirebaseAuth.instance.currentUser!;
+  final user = FirebaseAuth.instance.currentUser!;
 
   // sign user out
-  //void signUserOut() {
-  //  FirebaseAuth.instance.signOut();
-  //}
-
-  int currentIndex = 0;
-
-  void goToPage(index) {
-    setState(() {
-      currentIndex = index;
-    });
+  void signUserOut() {
+    FirebaseAuth.instance.signOut();
   }
-
-  final List _pages = [
-    // dummy search page
-    SearchPage(),
-
-    // dummy create page
-    CreatePage(),
-
-    // dummy travel plans page
-    TravelPlansPage(),
-
-    // dummy profile page
-    ProfilePage(),
-  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      //backgroundColor: Colors.grey[300],
-      //appBar: AppBar(
-      //  backgroundColor: Colors.grey[300],
-      //  actions: [
-      //    IconButton(
-      //      onPressed: signUserOut,
-      //      icon: const Icon(Icons.logout),
-      //    )
-      //  ],
-      //),
-      body: _pages[currentIndex],
-      //Center(
-      //    child: Text(
-      //  "LOGGED IN AS: ${user.email!}",
-      //  style: const TextStyle(fontSize: 20),
-      //)),
-      bottomNavigationBar: Container(
-        color: Colors.black,
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 20.0),
-          child: GNav(
-            onTabChange: (index) => goToPage(index),
-            backgroundColor: Colors.black,
-            color: Colors.white,
-            activeColor: Colors.white,
-            tabBackgroundColor: Colors.grey.shade800,
-            gap: 8,
-            padding: const EdgeInsets.all(16),
-            tabs: const [
-              GButton(icon: Icons.search, text: 'Search'),
-              GButton(icon: Icons.add_circle_outline, text: 'Create'),
-              GButton(icon: Icons.map_outlined, text: 'Travel Plans'),
-              GButton(icon: Icons.account_circle_outlined, text: 'Profile')
-            ],
-          ),
-        ),
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: signUserOut,
+            icon: Icon(Icons.logout),
+          )
+        ],
       ),
+      body: Center(
+          child: Text(
+        "LOGGED IN AS: " + user.email!,
+        style: TextStyle(fontSize: 20),
+      )),
     );
   }
 }
